d81c2b01d7ab5cd61dcacf171ffa5f35861a4213
bmengineer <bmengineer.gear@gmail.com>
2020-07-02 14:52:01 -0400

    Added auto-detection of test start, configurable terminal readouts, and configurable recording intervals

--- runtimetest.py
+++ runtimetest.py
@@ -1,6 +1,4 @@
 # runtimetest.py
-# made for bmengineer.com
-# Record TSL2591 sensor to csv
 from time import strftime, gmtime, sleep
 import time
 import math
@@ -14,11 +12,9 @@ import RPi.GPIO as GPIO
 import argparse
 import sys
 
-# Initialize the I2C bus
+# Initialize the I2C bus and the sensor, setup the LEDs
 i2c = busio.I2C(board.SCL, board.SDA)
-# Initialize the sensor
 sensor = adafruit_tsl2591.TSL2591(i2c)
-# Setup LED
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT) #RunningLED
@@ -38,6 +34,14 @@ def writereadingsgetlux(filename, t):
         writer = csv.writer(f, delimiter=",")
         writer.writerow([t, lux])
     return lux
+def statusLEDblink(state):
+    if state == 1:
+        GPIO.output(17, GPIO.HIGH)
+        state = 0
+    else:
+        GPIO.output(17, GPIO.LOW)
+        state = 1
+    return state
 def writingLEDblink(state):
     if state == 1:
         GPIO.output(27, GPIO.HIGH)
@@ -53,8 +57,12 @@ def timestamp():
 
 # Parse inputs
 parser = argparse.ArgumentParser()
-parser.add_argument('-o','--outputfile', help='filename for the csv output')
-parser.add_argument('-d','--duration',type=float,help='duration of the test in minutes')
+parser.add_argument('-o','--outputfile', help='Filename for the csv output. If left blank, filename will be a timestamp')
+parser.add_argument('-d','--duration',type=float,help='Duration of the test in minutes. If left blank, test will run for up to 24 hours.')
+parser.add_argument('-i','--interval',type=float,help='Delay between measurements in seconds. Will set to one second if not specified. Delay is halved during the 30s sampling period at the beginning of the test.')
+parser.add_argument('-tp','--terminationpercentage',type=float,help='Percentage to stop recording at. If left blank, test will run until the duration has passed.')
+parser.add_argument('-pp','--printpercentage',type=float,help="Percent change between printed updates to the terminal. By default, the output will print every 5%%. Set to 100 if you don't want to see these updates.")
+parser.add_argument('-pd','--printdelay',type=float,help="Minutes between printed updates to the terminal. By default, updates will be based on -printpercentage and not time.")
 args = parser.parse_args()
 if args.outputfile:
     filename = args.outputfile
@@ -67,9 +75,27 @@ if args.outputfile:
 else:
     filename = strftime("%Y%m%dTest.csv",gmtime())
 if args.duration:
-    testduration = args.duration*60.0
+    testduration = args.duration * 60.0
 else:
     testduration = 86400.0
+if args.interval:
+    delay = args.interval
+else:
+    delay = 1.0
+if args.terminationpercentage:
+    terminationpercentage = args.terminationpercentage
+    autoterm = 1
+else:
+    terminationpercentage = -1.0
+    autoterm = 0
+if args.printpercentage:
+    printpercentage = args.printpercentage
+else:
+    printpercentage = 5.0
+if args.printdelay:
+    printdelay = args.printdelay * 60.0
+else:
+    printdelay = testduration
 
 # Check for file conflicts
 if os.path.isfile(filename):
@@ -93,51 +119,76 @@ sensorceiling = 88000.0
 ANSIlux = sensorceiling
 nextprintpercent = 95
 tstart = time.time()
-t5seconds = tstart + 5.0
-t35seconds = tstart + 35.0
+
+# Get a baseline reading
+baselinesum = 0
+for i in range(5):
+    lux = sensor.lux
+    baselinesum += lux
+    time.sleep(1.0)
+baselinelux = baselinesum / 5
+thresholdlux = baselinelux * 3
+
+print ("{}Ready to start the test. Turn on the light now.".format(timestamp()))
+
+# Wait to detect some light at least triple the baseline
+statusstate = 0
+while lux < thresholdlux:
+    lux = sensor.lux
+    statusstate = statusLEDblink(statusstate)
+    time.sleep(0.5)
+
+print ("{}Light detected. Recording started.".format(timestamp()))
+GPIO.output(17, GPIO.HIGH)
+tstart = time.time()
+t30seconds = tstart + 30.0
 tterminate = tstart + testduration
 
-# Buffer to start test
-t = time.time()
-while t < t5seconds:
-    t = time.time()
-    lux = writereadingsgetlux(filename, t)
-    state = writingLEDblink(state)
-    time.sleep(1.0)
-
 # Sampling period where max is set
-while t < t35seconds:
+samplingluxmin = sensorceiling
+samplingluxmax = 0.0
+t = time.time()
+while t < t30seconds:
     t = time.time()
     lux = writereadingsgetlux(filename, t)
     state = writingLEDblink(state)
-    if lux < ANSIlux:
-        ANSIlux = lux
-    time.sleep(1.0)
+    if lux < samplingluxmin:
+        samplingluxmin = lux
+    if lux > samplingluxmax:
+        samplingluxmax = lux
+    time.sleep(delay/2)
+ANSIlux = lux
 if ANSIlux == 88000.0:
-    print("{}Sensor is saturated. The light is too bright to measure with your current setup.".format(timestamp()))
+    print("{}Sensor is saturated. The light is too bright to measure with your current setup. Consider adding a filter between the source and the sensor. The test will continue, but will be cut off at the high end.".format(timestamp()))
+
+print("{}Sampling period complete. The output at 30s was {:.1f} lux. Sampling period max = {:.1f} lux, min = {:.1f} lux.".format(timestamp(), ANSIlux, samplingluxmax, samplingluxmin))
+testdurationminutes = testduration / 60.0
+cutofflux = ANSIlux * terminationpercentage / 100.0
+if autoterm == 1:
+    print ("\tThe test will run for {:.0f} minutes, or until it reaches {:.1f} lux ({}% of the output at 30s). Or until you stop it.".format(testdurationminutes, cutofflux, terminationpercentage))
+else:
+    print("\tThe test will run for {:.0f} minutes, or until you stop it.".format(testdurationminutes))
 
 # Main recording code
-print ("{}ANSI lux = {}".format(timestamp(),ANSIlux))
+printedpercent = 100
+nextprinttime = t + printdelay
 while t < tterminate:
     t = time.time()
     lux = writereadingsgetlux(filename, t)
     state = writingLEDblink(state)
-    percentofANSIlux=lux/ANSIlux*100
-    if percentofANSIlux < nextprintpercent:
-        tsincestart = t - tstart
-        print("{}{:.0f}% of {:.0f} ({:.0f})".format(timestamp(),percentofANSIlux, ANSIlux, lux))
-        while nextprintpercent > percentofANSIlux:
-            nextprintpercent -= 5
-        if nextprintpercent < 10:#Runtime test is done, measure for a bit longer then stop the test
-            GPIO.output(22, GPIO.HIGH)
-            tremaining = (t - tstart)/10.0
-            if tremaining > 3600.0:
-                tremaining = 3600.0
-            if (tstart+tremaining) < tterminate:
-                tterminate = t+tremaining
-            minutesremaining = tremaining/60.0
-            print("{}Test will end in {:.1f} minutes".format(timestamp(),minutesremaining))
-    time.sleep(1.0)
+    percentofANSIlux = lux / ANSIlux * 100.0
+    if autoterm == 1 and percentofANSIlux <= terminationpercentage:
+        autoterm = 0
+        tremaining = (t - tstart) * 0.05
+        if (t + tremaining) < tterminate:
+            tterminate = t + tremaining
+        minutesremaining = (tterminate - t) / 60.0
+        print("{}Output has reached {:.0f}% ({:.0f} lux), which is at or below your {}% target. The test will run for {:.1} minutes before ending itself.".format(timestamp(), percentofANSIlux, lux, terminationpercentage, minutesremaining))
+    elif t >= nextprinttime or abs(percentofANSIlux - printedpercent) >= printpercentage:
+        print("{}Output is at {:.0f}% ({:.0f} lux)".format(timestamp(),percentofANSIlux, lux))
+        printedpercent = percentofANSIlux
+        nextprinttime = t + printdelay
+    time.sleep(delay)
 print("{}Test complete".format(timestamp()))
 GPIO.output(17, GPIO.LOW)
 GPIO.output(22, GPIO.LOW)
