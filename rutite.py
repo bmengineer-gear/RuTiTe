b1043709097cd608de32c33efc680aef31348368
bmengineer-gear <66273616+bmengineer-gear@users.noreply.github.com>
2020-07-10 14:43:37 -0400

    Update and rename runtimetest.py to rutite.py
    - Added the option to plot, save absolute time, convert lumens to lux
    - renamed

--- rutite.py
+++ rutite.py
@@ -13,27 +13,41 @@ import adafruit_tsl2591
 import RPi.GPIO as GPIO
 import argparse
 import sys
+import matplotlib.pyplot as plt
+
+ready_led = 17
+running_led = 27
+complete_led = 22
+sensor_ceiling = 88000.0
+lux_to_lumen_factor = 1.0
 
 def init():
     i2c = busio.I2C(board.SCL, board.SDA)
     sensor = adafruit_tsl2591.TSL2591(i2c)
     GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)  
-    ready_led = 17
-    running_led = 27
-    complete_led = 22
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(ready_led, GPIO.OUT)
     GPIO.setup(running_led, GPIO.OUT)
     GPIO.setup(complete_led, GPIO.OUT)
     GPIO.output(ready_led, GPIO.HIGH)
     GPIO.output(running_led, GPIO.LOW)
     GPIO.output(complete_led, GPIO.LOW)
-    return ready_led, running_led, complete_led
+    return sensor
 
-def write_to_csv(filename, t, lux):
-    with open (filename, "a") as f:
+def write_to_csv(options, t, lux, t_test_start):
+    if options.absolute_time:
+        t_abs = t - t_test_start
+    else:
+        t_abs = ''
+    
+    if options.lux_to_lumen_factor:
+        lumens = lux * options.lux_to_lumen_factor
+    else:
+        lumens = ''
+    
+    with open (options.filename, "a") as f:
         writer = csv.writer(f, delimiter=",")
-        writer.writerow([t, lux])
+        writer.writerow([t, lux, t_abs, lumens])
 
 def blink_led(pin):
     GPIO.output(pin, not GPIO.input(pin))
@@ -43,12 +57,29 @@ def current_timestamp():
 
 def build_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o','--outputfile', dest='filename', default=time.strftime('RuTiTe%Y-%m-%d-%H.%M.%S.csv', time.localtime()),help='filename for the csv output')
-    parser.add_argument('-i','--interval', dest='delay', type=float, default=1.0, help='interval between measurements in seconds (halved during the 30s sampling period at the beginning of the test)')
-    parser.add_argument('-d','--duration', type=float, help='maximum duration of the test in minutes')
-    parser.add_argument('-tp','--termination-percentage', dest='termination_percentage', type=float, default=10.0, help='percent output to stop recording at')
-    parser.add_argument('-pp','--print-percentage', dest='percent_change_to_print', type=float, default=5.0, help='percent change between printed updates to the terminal')
-    parser.add_argument('-pd','--print-delay', dest='time_between_prints', type=float, help='minutes between printed updates to the terminal')
+    parser.add_argument('-o','--outputfile', dest='filename', 
+            default=time.strftime('RuTiTe%Y-%m-%d-%H.%M.%S.csv', time.localtime()),
+            help = 'filename for the csv output')
+    parser.add_argument('-i','--interval', dest='delay', type=float, 
+            default = 1.0, 
+            help = 'interval between measurements in seconds (halved during the 30s sampling period at the beginning of the test)')
+    parser.add_argument('-d','--duration', dest='test_duration', type=float, 
+            help = 'maximum duration of the test in minutes')
+    parser.add_argument('-tp','--termination-percentage', dest='termination_percentage', type=float, 
+            default = 10.0, 
+            help = 'percent output to stop recording at')
+    parser.add_argument('-pp','--print-percentage', dest='percent_change_to_print', type=float, 
+            default = 5.0, 
+            help = 'percent change between printed updates to the terminal')
+    parser.add_argument('-pd','--print-delay', dest='time_between_prints', type=float, 
+            help = 'minutes between printed updates to the terminal')
+    parser.add_argument('-lf', '--lux-to-lumen-factor', dest='lux_to_lumen_factor', type=float, 
+            default=lux_to_lumen_factor, 
+            help = 'lux to lumen conversion factor for use in calibrated integrating enclosures')
+    parser.add_argument('-a', '--absolute-time', dest='absolute_time',
+            help = 'record absolute time, with the first measurement at t=0')
+    parser.add_argument('-g', '--graph-title', dest='graph_title',
+            help = 'string to use for a basic plot of the recorded data - only works if you let the script run until it stops based on time or percent output')
     return parser
 
 def load_options():
@@ -56,6 +87,8 @@ def load_options():
     options = parser.parse_args()
     if options.time_between_prints:
         options.time_between_prints *= 60
+    if options.test_duration:
+        options.test_duration *= 60
     if os.path.isfile(options.filename):
         print ("{}{} already exists. Checking for an an available name to avoid overwriting something important...".format(current_timestamp(), options.filename))
         options.filename = time.strftime('RuTiTe%Y-%m-%d-%H.%M.%S.csv', time.localtime())
@@ -65,36 +98,35 @@ def load_options():
 def add_csv_header(filename):
     with open (filename, "a") as f:
         writer = csv.writer(f, delimiter=",")
-        writer.writerow(["time", "lux"])
+        writer.writerow(["time", "lux", "absolute time", "lumens"])
         blink_led(running_led)
 
-def core(options):
-    sensor_ceiling = 88000.0 
+def core(options, sensor):
     state = 'set_baseline'
     baseline_sum = 0.0
     baseline_measurement_count = 0
     ceiling_reached = False
-    
+
     while state != 'exit':
         lux = sensor.lux
         t = time.time()
-        
+
         if lux == sensor_ceiling and ceiling_reached == False:
             print("{}Sensor is saturated. The light is too bright to measure with your current setup. Consider adding a filter between the source and the sensor. The test will continue, but will be cut off at the high end.".format(current_timestamp()))
             ceiling_reached = True
-        
+
         if state == 'set_baseline':
             baseline_measurement_count += 1
             baseline_sum += lux
-            
+
         if state == 'waiting_for_threshold':
             blink_led(ready_led)
             time.sleep(0.5)
 
         if state in ['sampling_period', 'main_recording']:
-            write_to_csv(options.filename, t, lux)
+            write_to_csv(options, t, lux, t_test_start)
             blink_led(running_led)
-            
+
         if state == 'sampling_period':
             if lux < sampling_lux_min:
                 sampling_lux_min = lux
@@ -104,7 +136,7 @@ def core(options):
 
         if state == 'main_recording':
             percent_output = lux / lux_at_30s * 100.0
-            
+
             if options.time_between_prints and (t - last_print_time) > options.time_between_prints:
                 print("{}Output is at {:.0f}% ({:.0f} lux)".format(current_timestamp(),percent_output, lux))
                 last_print_time = t
@@ -113,69 +145,103 @@ def core(options):
                 print("{}Output is at {:.0f}% ({:.0f} lux)".format(current_timestamp(),percent_output, lux))
                 last_print_time = t
                 last_printed_percent = percent_output
-                
+
         if state in ['set_baseline', 'waiting_for_threshold', 'sampling_period'] and options.delay > 0.5:
             time.sleep(0.5)
         else:
             time.sleep(options.delay)
-        
+
         if state == 'set_baseline' and baseline_measurement_count >= 5:
             threshold_lux = baseline_sum / baseline_measurement_count * 3.0
             state = 'waiting_for_threshold'
             print ("{}Ready to start the test. Turn on the light now.".format(current_timestamp()))
-                
+
         if state == 'waiting_for_threshold' and lux >= threshold_lux:
             state = 'sampling_period'
             GPIO.output(ready_led, GPIO.HIGH)
             t_test_start = time.time()
             t_sampling_complete = t_test_start + 30.0
-            t_test_complete = t_test_start + test_duration
+            if options.test_duration:
+                t_test_complete = t_test_complete = t_test_start + options.test_duration
             sampling_lux_min = sensor_ceiling
             sampling_lux_max = 0.0
             print ("{}Light detected. Recording started.".format(current_timestamp()))
             time.sleep(1.0)
-        
+
         if state == 'sampling_period' and t >= t_sampling_complete:
             state = 'main_recording'
-            lux_at_30s = lux                    
+            lux_at_30s = lux
             print("{}Sampling period complete. The output at 30s was {:.1f} lux. Sampling period max = {:.1f} lux, min = {:.1f} lux.".format(current_timestamp(), lux_at_30s, sampling_lux_max, sampling_lux_min))
-                text_to_print = '\tThe test will run until you stop it'
+            text_to_print = '\tThe test will run until you stop it'
             if options.test_duration:
                 text_to_print += ', or it has recorded for {:.0f} minutes'.format(options.test_duration/60)
             if options.termination_percentage:
                 termination_output = lux_at_30s * options.termination_percentage / 100
-                text_to_print += ', or it reaches {:.1f} lux ({}% of the output at 30s)'.format(termination_output, options.termination_percentage)
+                text_to_print += ', or it reaches {:.1f} lux ({:.1f}% of the output at 30s)'.format(termination_output, options.termination_percentage)
             print(text_to_print + '.')
             last_printed_percent = 100.0
             last_print_time = t
-        
+            percent_output = 100.0
+
         if state == 'main_recording':
-            if options.termination_percentage and percent_output <= termination_percentage:
-                options.termination_percentage = False
+            if options.termination_percentage and percent_output <= options.termination_percentage:
                 t_remaining = (t - t_test_start) * 0.05
                 print("{}Output has reached {:.0f}% ({:.0f} lux), which is at or below your {}% target.".format(current_timestamp(), percent_output, lux, options.termination_percentage))
                 last_print_time = t
                 last_printed_percent = percent_output
+                options.termination_percentage = False
                 if options.test_duration and (t + t_remaining) > options.test_duration:
                     t_remaining = options.test_duration - t
                 else:
                     t_test_complete = t + t_remaining
                     options.test_duration = t - t_remaining + t_remaining
                 print("\tJust collecting a bit more data. The test will end in {:.1f} minutes.".format(t_remaining/60))
-        
             if options.test_duration and t >= t_test_complete:
                 state = 'exit'
-                
+
     print("{}Test complete".format(current_timestamp()))
     GPIO.output(ready_led, GPIO.LOW)
     GPIO.output(running_led, GPIO.LOW)
     GPIO.output(complete_led, GPIO.HIGH)
 
+def runtimeplot(options):
+    
+    fig = plt.figure(figsize=(15, 10))
+    
+    time = []
+    brightness = []
+
+    with open(options.filename, 'r') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        next(data)
+        for row in data:
+            time.append(float(row[0]))
+            if options.lux_to_lumen_factor == 1.0:
+                brightness.append(float(row[1]))
+            else:
+                brightness.append(float(row[3]))
+    
+    t_test_start = time[1]
+    time = [(x - t_test_start) / 60 for x in time]
+    plt.plot(time, brightness)
+    plt.xlabel('Time [minutes]')
+    if options.lux_to_lumen_factor == 1.0:
+        plt.ylabel('Output [detected lux]')
+    else:
+        plt.ylabel('Output [calculated lumens]')
+    plt.title(options.graph_title)
+    plt.grid(True)
+    plt.xlim(left=0)
+    plt.ylim(bottom=0)
+    plt.savefig(options.graph_title+'.png')
+
 def main():
-    init()
+    sensor = init()
     options = load_options()
     add_csv_header(options.filename)
-    core(options)
-    
+    core(options, sensor)
+    if options.graph_title:
+        runtimeplot(options)
+
 if __name__ == "__main__":
     main()
